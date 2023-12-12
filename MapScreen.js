e28780c4127ff9ed31c5543cb476cbf4a4a58923
Karen Ramos <karen.ramos.r@tecsup.edu.pe>
2023-12-12 14:43:38 -0500

    primera vr

---
+++ MapScreen.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+
+const MapScreen = () => {
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.error('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  return (
+    <View style={{ flex: 1 }}>
+      {location ? (
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            title="You are here"
+            description="Your current location"
+          />
+        </MapView>
+      ) : (
+        <Text>Loading...</Text>
+      )}
+    </View>
+  );
+};
+
+export default MapScreen;
